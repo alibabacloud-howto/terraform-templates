a2d290d89309ab9c6b3c3b7ad9292a0639174d56
Julian Zhou <junlan.zx@alibaba-inc.com>
2021-05-26 14:58:27 +0800

    Add database alwaysfree TF scripts.

---
+++ database-alwaysfree/README.md
@@ -0,0 +1,17 @@
+# PolarDB Always Free
+Please check here with detailed information:
+[https://www.alibabacloud.com/product/databases?J_2463051000&anchor=entryProducts2](https://www.alibabacloud.com/product/databases?J_2463051000&anchor=entryProducts2)
+
+PolarDB is compute and storage decoupled. These Terraform scripts only provision the compute cluster (primary node and read-only node). For 50GB free storage package every month, you can get it here: [https://www.alibabacloud.com/product/polardb?#J_5764451150](https://www.alibabacloud.com/product/polardb?#J_5764451150)
+
+- Always free PolarDB is only available at regions below, please select one of these regions.
+  - US(Silicon Valley): us-west-1 
+  - Singapore: ap-southeast-1
+  - UK(London): eu-west-1
+  - Australia(Sydney): ap-southeast-2
+  - China(Hong Kong): cn-hongkong
+
+The terraform scripts are:
+- PolarDB MySQL: 
+- PolarDB PostgreSQL:
+- PolarDB Oracle:
---
+++ database-alwaysfree/polardb-postgresql/main.tf
@@ -0,0 +1,54 @@
+provider "alicloud" {
+  #   access_key = "${var.access_key}"
+  #   secret_key = "${var.secret_key}"
+
+  # Always free PolarDB is only available at regions: 
+  # US(Silicon Valley): us-west-1 
+  # Singapore: ap-southeast-1
+  # UK(London): eu-west-1
+  # Australia(Sydney): ap-southeast-2
+  # China(Hong Kong): cn-hongkong
+  # 
+  # Please select one of these regions.
+  region = "cn-hongkong"
+}
+
+variable "polardb_postgresql_name" {
+  default = "polardb_postgresql"
+}
+
+variable "creation" {
+  default = "PolarDB"
+}
+
+data "alicloud_zones" "default" {
+  available_resource_creation = var.creation
+}
+
+resource "alicloud_vpc" "default" {
+  vpc_name   = "vpc-test"
+  cidr_block = "172.16.0.0/16"
+}
+
+resource "alicloud_vswitch" "default" {
+  vpc_id       = alicloud_vpc.default.id
+  cidr_block   = "172.16.0.0/24"
+  zone_id      = data.alicloud_zones.default.zones[0].id
+  vswitch_name = "vsw-test"
+}
+
+resource "alicloud_polardb_cluster" "cluster" {
+  db_type       = "PostgreSQL"
+  db_version    = "11"
+  db_node_class = "polar.pg.x4.medium" # 2C8G dedicated
+  pay_type      = "PostPaid"           # Need to be PrePaid
+  vswitch_id    = alicloud_vswitch.default.id
+  description   = var.polardb_postgresql_name
+}
+
+resource "alicloud_polardb_account" "account" {
+  db_cluster_id       = alicloud_polardb_cluster.cluster.id
+  account_name        = "test_polardb"
+  account_password    = "N1cetest"
+  account_description = var.polardb_postgresql_name
+}
