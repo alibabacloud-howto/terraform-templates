9a2143307b065766ed575a114f9f12d30673e7a8
Julian Zhou <junlan.zx@alibaba-inc.com>
2021-12-03 16:39:35 +0800

    Add polardbm-adbmysql

--- terraform-alicloud-db-samples/dts/data_synchronization/polardbm-adbmysql/main.tf
+++ terraform-alicloud-db-samples/dts/data_synchronization/polardbm-adbmysql/main.tf
@@ -61,25 +61,25 @@ resource "alicloud_polardb_account_privilege" "source_privilege" {
   db_names          = [alicloud_polardb_database.source_db.db_name]
 }
 
-## PolarDB MySQL Target
-resource "alicloud_polardb_cluster" "target" {
-  db_type       = "MySQL"
-  db_version    = "8.0"
-  db_node_class = "polar.mysql.g2.medium"
-  pay_type      = "PostPaid"
-  vswitch_id    = alicloud_vswitch.default.id
-  description   = "PolarDB MySQL as DTS Target"
-  parameters {
-    name  = "loose_polar_log_bin"
-    value = "ON"
-  }
+## AnalyticDB MySQL Target
+resource "alicloud_adb_db_cluster" "target" {
+  db_cluster_category = "MixedStorage"
+  compute_resource    = "32Core128GB"
+  # elastic_io_resource = "2"
+  mode               = "flexible"
+  db_cluster_version = "3.0"
+  payment_type       = "PayAsYouGo"
+  vswitch_id         = alicloud_vswitch.default.id
+  description        = "AnalyticDB in elastic mode."
+  maintain_time      = "23:00Z-00:00Z"
+  # security_ips       = ["10.168.1.12", "10.168.1.11"]
 }
 
-resource "alicloud_polardb_account" "target_account" {
-  db_cluster_id       = alicloud_polardb_cluster.target.id
-  account_name        = "test_polardb"
+resource "alicloud_adb_account" "account" {
+  db_cluster_id       = alicloud_adb_db_cluster.target.id
+  account_name        = "test_adb"
   account_password    = "N1cetest"
-  account_description = "PolarDB MySQL as DTS Target"
+  account_description = "AnalyticDB MySQL as DTS Source"
 }
 
 ## DTS Data Synchronization
@@ -87,16 +87,15 @@ resource "alicloud_dts_synchronization_instance" "default" {
   payment_type                     = "PayAsYouGo"
   source_endpoint_engine_name      = "PolarDB"
   source_endpoint_region           = var.region
-  destination_endpoint_engine_name = "PolarDB"
+  destination_endpoint_engine_name = "ADB30"
   destination_endpoint_region      = var.region
   instance_class                   = "small"
-  sync_architecture                = "bidirectional"
+  sync_architecture                = "oneway"
 }
 
-resource "alicloud_dts_synchronization_job" "forward" {
-  synchronization_direction          = "Forward"
+resource "alicloud_dts_synchronization_job" "default" {
   dts_instance_id                    = alicloud_dts_synchronization_instance.default.id
-  dts_job_name                       = "tf-polardbm-polardbm"
+  dts_job_name                       = "tf-polardbm-adbmysql"
   source_endpoint_instance_type      = "PolarDB"
   source_endpoint_instance_id        = alicloud_polardb_cluster.source.id
   source_endpoint_engine_name        = "PolarDB"
@@ -104,38 +103,13 @@ resource "alicloud_dts_synchronization_job" "forward" {
   source_endpoint_database_name      = "test_database"
   source_endpoint_user_name          = alicloud_polardb_account.source_account.account_name
   source_endpoint_password           = alicloud_polardb_account.source_account.account_password
-  destination_endpoint_instance_type = "PolarDB"
-  destination_endpoint_instance_id   = alicloud_polardb_cluster.target.id
-  destination_endpoint_engine_name   = "PolarDB"
+  destination_endpoint_instance_type = "ads"
+  destination_endpoint_instance_id   = alicloud_adb_db_cluster.target.id
+  destination_endpoint_engine_name   = "ADB30"
   destination_endpoint_region        = var.region
   destination_endpoint_database_name = "test_database"
-  destination_endpoint_user_name     = alicloud_polardb_account.target_account.account_name
-  destination_endpoint_password      = alicloud_polardb_account.target_account.account_password
-  db_list                            = "{\"test_database\":{\"name\":\"test_database\",\"all\":true}}"
-  structure_initialization           = "true"
-  data_initialization                = "true"
-  data_synchronization               = "true"
-  status                             = "Synchronizing"
-}
-
-resource "alicloud_dts_synchronization_job" "reverse" {
-  synchronization_direction          = "Reverse"
-  dts_instance_id                    = alicloud_dts_synchronization_instance.default.id
-  dts_job_name                       = "tf-polardbm-polardbm"
-  source_endpoint_instance_type      = "PolarDB"
-  source_endpoint_instance_id        = alicloud_polardb_cluster.target.id
-  source_endpoint_engine_name        = "PolarDB"
-  source_endpoint_region             = var.region
-  source_endpoint_database_name      = "test_database"
-  source_endpoint_user_name          = alicloud_polardb_account.target_account.account_name
-  source_endpoint_password           = alicloud_polardb_account.target_account.account_password
-  destination_endpoint_instance_type = "PolarDB"
-  destination_endpoint_instance_id   = alicloud_polardb_cluster.source.id
-  destination_endpoint_engine_name   = "PolarDB"
-  destination_endpoint_region        = var.region
-  destination_endpoint_database_name = "test_database"
-  destination_endpoint_user_name     = alicloud_polardb_account.source_account.account_name
-  destination_endpoint_password      = alicloud_polardb_account.source_account.account_password
+  destination_endpoint_user_name     = alicloud_adb_account.account.account_name
+  destination_endpoint_password      = alicloud_adb_account.account.account_password
   db_list                            = "{\"test_database\":{\"name\":\"test_database\",\"all\":true}}"
   structure_initialization           = "true"
   data_initialization                = "true"
