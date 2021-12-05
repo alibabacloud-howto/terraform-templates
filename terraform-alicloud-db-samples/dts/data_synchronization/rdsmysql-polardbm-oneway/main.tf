8b964f26dd51045f991250e846798795b962e8ca
Julian Zhou <junlan.zx@alibaba-inc.com>
2021-12-05 10:52:10 +0800

    Update rdsmysql-polardbm-oneway

--- terraform-alicloud-db-samples/dts/data_synchronization/rdsmysql-polardbm-oneway/main.tf
+++ terraform-alicloud-db-samples/dts/data_synchronization/rdsmysql-polardbm-oneway/main.tf
@@ -78,7 +78,7 @@ resource "alicloud_dts_synchronization_instance" "default" {
   payment_type                     = "PayAsYouGo"
   source_endpoint_engine_name      = "MySQL"
   source_endpoint_region           = var.region
-  destination_endpoint_engine_name = "MySQL"
+  destination_endpoint_engine_name = "PolarDB"
   destination_endpoint_region      = var.region
   instance_class                   = "small"
   sync_architecture                = "oneway"
